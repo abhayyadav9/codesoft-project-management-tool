14e8289c6510ea97396bbb8d6eb02c4c69b30fcb
Abhay yadav <yadavabhay8227.com>
2024-08-12 17:49:46 +0530

    update git

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const corsOptions = {
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -19,6 +20,12 @@ app.use("/api/", UserAPI);
 app.use("/api", TaskAPI);
 
 const PORT = process.env.PORT || 5000; // Use environment variable for port if available
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
